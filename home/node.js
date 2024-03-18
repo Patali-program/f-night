5bf1614f77bda9b500dc1abdf595a0b2427d6d15
Pratyush0898 <pkcanva123@gmail.com>
2024-03-18 14:18:08 +0530

    Updated changes

--- home/node.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById("intro").style.transform = "translateY(-100%)";
-        document.getElementById("main").style.opacity = "1";
-    }, 2000); // Adjust the delay (in milliseconds) as needed
-})
