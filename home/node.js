a5fa23ccdf71b2d1eb944952ac1f890d844380a5
nvmPratyush <142408079+nvmPratyush@users.noreply.github.com>
2024-03-18 14:14:06 +0530

    Add files via upload

---
+++ home/node.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("intro").style.transform = "translateY(-100%)";
+        document.getElementById("main").style.opacity = "1";
+    }, 2000); // Adjust the delay (in milliseconds) as needed
+})
