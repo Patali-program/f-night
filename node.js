a54e8a429e5a4fcbec9177104c9c1a45cb11f953
Pratyush0898 <pkcanva123@gmail.com>
2024-03-19 11:54:54 +0530

    Updated node.js

--- node.js
+++ node.js
@@ -1,6 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById("intro").style.transform = "translateY(-100%)";
-        document.getElementById("main").style.opacity = "1";
-    }, 2000); // Adjust the delay (in milliseconds) as needed
-})
+window.addEventListener('contextmenu', (e)=> {
+    e.preventDefault();
+  });
+  const r = () => {
+    return false;
+  }
+  document.onkeydown = function(e)
+  {
+    if (e.keyCode == 123)
+    {
+        r()
+    }
+  
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+    {
+        r()
+    }
+  
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+    {
+        r()
+    }
+  
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+    {
+        r()
+    }
+  }
